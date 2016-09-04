9baec39fc7390846d13d50f239b1ad4772055c69
J-C-Wright <jack.charles.wright@cern.ch>
2016-09-04 17:32:07 +0200

    Adds canon class and adds various class operator overloads

--- HexFramework/CanonAddress.py
+++ HexFramework/CanonAddress.py
@@ -1,5 +1,79 @@
 
-from bitstring import Bits
-from bitstring import BitArray
 from math import pow
 
+import StandardAddress as sa
+
+class CanonAddress:
+
+    def __init__(self,address):
+        assert address.isdigit(),'Must give an integer input'
+
+        self.address = address
+
+    def __str__(self):
+        return self.address
+
+    def __add__(self,other):
+
+        standard_self = sa.standardFromCanon(self)
+        standard_other = sa.standardFromCanon(other)
+        return canonFromStandard(standard_self + standard_other)
+
+    def __sub__(self,other):
+
+        standard_self = sa.standardFromCanon(self)
+        standard_other = sa.standardFromCanon(other)
+        return canonFromStandard(standard_self - standard_other)
+
+    
+
+
+
+
+
+
+def canonFromStandard(address):
+
+    standard = address.bin
+    canonStr = ''
+
+    for i in range(len(address)/3):
+        digit = standard[0+i*3:3+i*3]
+        if digit == '100':
+            canonStr += '1'
+        elif digit == '010':
+            canonStr += '2'
+        elif digit == '110':
+            canonStr += '3'
+        elif digit == '001':
+            canonStr += '4'
+        elif digit == '101':
+            canonStr += '5'
+        elif digit == '011':
+            canonStr += '6'
+        else: 
+            canonStr += '0'  #111 = 000
+           
+    return CanonAddress(canonStr)
+        
+def canonFromSpiral(address):
+
+    length = len(str(address))
+    canonStr = ''
+
+    for i in range(length,-1,-1):
+        rem = address % 7**i
+        if (rem != address):
+            canonStr += str((address - rem)/(7**i))
+            address = rem
+        else:
+            canonStr += '0'
+    if canonStr[0] == '0':
+        return CanonAddress(canonStr[1:])  #Fix this
+    else:
+        return CanonAddress(canonStr)
+
+   
+    
+    
+    
--- HexFramework/StandardAddress.py
+++ HexFramework/StandardAddress.py
@@ -1,6 +1,101 @@
 from bitstring import Bits
 from bitstring import BitArray
 from math import pow
+import CanonAddress as ca
+
+class StandardAddress(Bits):
+
+    #Adds an assert to make sure that the digits have length 3
+    def __init__(self, *args, **kwargs):
+        super(StandardAddress, self).__init__(*args, **kwargs)
+        assert len(self.bin)%3 == 0, 'Length of address must be a multiple of three.'
+    
+    #Addition overload
+    def __add__(self,other):
+        
+        while (len(self) != len(other)):
+            if (len(self) < len(other)):
+                self = Bits(bin='000'+self.bin)
+            else:
+                other = Bits(bin='000'+other.bin)
+
+        digits1 = []
+        digits2 = []
+
+        for i in range(len(self)/3):
+            digits1.append(self[0+i*3:3+i*3])
+            digits2.append(other[0+i*3:3+i*3])
+        
+        digits1.reverse()
+        digits2.reverse()
+
+        addressOut = []
+        carries = []
+        for digit1,digit2 in zip(digits1,digits2):
+
+            digits = [digit1,digit2] + carries
+
+            carries = []
+            rem = Bits(bin='000')
+            for digit in digits:
+
+                remtemp = remainderDigits(rem,digit)
+                car = carryDigits([rem,digit,remtemp])
+                rem = remtemp
+                carries.append(car)
+
+            addressOut.append(rem)
+        
+        addressOut.reverse()
+        bits = BitArray('')
+
+        #If there are still non-zero carries
+        if not all(c.bin == '000' or c.bin == '111' for c in carries):
+            rem = Bits(bin='000')
+            for digit in carries:
+                remtemp = remainderDigits(rem,digit)
+                car = carryDigits([rem,digit,remtemp])
+                rem = remtemp
+            if rem.bin != '000' and rem.bin != '111': bits.append(rem)
+
+        for digit in addressOut:
+            bits.append(digit)
+
+        return bits
+
+    #Subtraction overload
+    def __sub__(self,other):
+        return self + ~other
+
+    #String overload method
+    def __str__(self):
+        return self.bin
+
+def standardFromCanon(canon):
+
+    binString = ''
+    for digit in canon.address:
+        if digit == '0':
+            binString += '000'
+        elif digit == '1':
+            binString += '100'
+        elif digit == '2':
+            binString += '010'
+        elif digit == '3':
+            binString += '110'
+        elif digit == '4':
+            binString += '001'
+        elif digit == '5':
+            binString += '101'
+        elif digit == '6':
+            binString += '011'
+
+    return StandardAddress(bin=binString)
+
+def standardFromSpiral(spiral):
+
+    canon = ca.canonFromSpiral(spiral)
+    return standardFromCanon(canon)
 
 def addDigits(digit1,digit2):
 
@@ -8,7 +103,7 @@ def addDigits(digit1,digit2):
     wrapCarry = False
 
     for i in range(3):
-        xoxrBit = digit1.bin[i] != digit2.bin[i]
+        xorBit = digit1.bin[i] != digit2.bin[i]
         remainder = xorBit != wrapCarry
 
         if wrapCarry:
@@ -20,10 +115,7 @@ def addDigits(digit1,digit2):
 
     return [wrapCarry,bits]
 
-
 def remainderDigits(digit1,digit2):
-
-    assert len(digit1) == 3 and len(digit2) ==3, 'Digit bitstring must always be length three'
 
     wrapCarry,bits = addDigits(digit1,digit2)
 
@@ -35,59 +127,9 @@ def remainderDigits(digit1,digit2):
 
 def carryDigits(digits):
 
-    for digit in digits:
-        assert len(digit) == 3, 'Digit bitstring must always be length three'
-
     bits = BitArray(bin='000')
     for digit in digits:
         bits = bits ^ digit
     bits.rol(1)
     return bits
-
-def addAddresses(address1,address2):
-
-    assert len(address1)%3 == 0 and len(address2)%3 == 0, "Bitstring must be made up of digits with three bits"
-
-    while (len(address1) != len(address2)):
-        if (len(address1) < len(address2)):
-            address1 = Bits(bin='000'+address1.bin)
-        else:
-            address2 = Bits(bin='000'+address2.bin)
-
-    numDigits = len(address1)/3
-
-    digits1 = []
-    digits2 = []
-
-    for i in range(numDigits):
-        digits1.append(address1[0+i*3:3+i*3])
-        digits2.append(address2[0+i*3:3+i*3])
-
-    digits1.reverse()
-    digits2.reverse()
-
-    addressOut = []
-    car = Bits(bin='000')
-    for i in range(numDigits):
-
-        rem = remainderDigits(digits1[i],digits2[i])
-        rem = remainderDigits(rem,car)
-        testcar = remainderDigits(car,rem)
-        car = carryDigits([testcar,digits1[i],digits2[i]])
-
-        addressOut.append(rem)
-
-    addressOut.reverse()
-    bits = BitArray('')
-
-    if (car.bin != '000' and car.bin != '111'):
-        bits.append(car)
-    for i in range(numDigits):
-        bits.append(addressOut[i])
-
-    return bits
-
-
-
-
 
