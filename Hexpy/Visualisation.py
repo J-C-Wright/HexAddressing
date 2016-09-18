44e57041645674db5b78be7178bafea8691ab200
J-C-Wright <jack.charles.wright@cern.ch>
2016-09-18 19:01:53 +0200

    Early work on visualisation submodule

--- Hexpy/Visualisation.py
+++ Hexpy/Visualisation.py
@@ -1,17 +1,35 @@
+import os
+import inspect
+
+from math import sqrt
 
 import matplotlib.pyplot as plt
-from matplotlib.path import path
+from matplotlib.path import Path
 import matplotlib.patches as patches
 
-import os
+import StandardAddress as sa
+import CanonAddress as ca
+import SpiralAddress as spa
 
-from Hexpy import StandardAddress as sa
-from Hexpy import CanonAddress as ca
-from Hexpy import SpiralAddress as spa
+from CanonAddress import CanonAddress as Canon
+from StandardAddress import StandardAddress as Standard
+from SpiralAddress import SpiralAddress as Spiral
 
-from Hexpy.CanonAddress import CanonAddress as Canon
-from Hexpy.StandardAddress import StandardAddress as Standard
-from Hexpy.SpiralAddress import SpiralAddress as Spiral
+import Coordinates as coords
+
+
+
+
+fig = plt.figure()
+fig.set_canvas(plt.gcf().canvas)
+ax = fig.add_subplot(111)
+ax.add_patch(patch)
+ax.set_xlim(-2.0,2.0)
+ax.set_ylim(-2.0,2.0)
+plt.axes().set_aspect('equal')
+
+plt.show()
+
 
 
 
