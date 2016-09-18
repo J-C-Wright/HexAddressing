2accf3acc72e7ebaa3af592e882c12fe9c80aaa2
J-C-Wright <jack.charles.wright@cern.ch>
2016-09-18 13:55:38 +0200

    Hex visualisation initial commit'

---
+++ Hexpy/Visualisation.py
@@ -0,0 +1,18 @@
+
+import matplotlib.pyplot as plt
+from matplotlib.path import path
+import matplotlib.patches as patches
+
+import os
+
+from Hexpy import StandardAddress as sa
+from Hexpy import CanonAddress as ca
+from Hexpy import SpiralAddress as spa
+
+from Hexpy.CanonAddress import CanonAddress as Canon
+from Hexpy.StandardAddress import StandardAddress as Standard
+from Hexpy.SpiralAddress import SpiralAddress as Spiral
+
+
+
+
